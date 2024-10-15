cec502548b816d9fa2d047435e1dce452f645b7d
gursheyss <rkwarya@gmail.com>
2024-10-14 18:12:16 -0700

    remove tutorial stuff

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   dialect: "postgresql",
-  schema: "./src/lib/db/schema.ts",
+  schema: "",
   out: "./drizzle",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
