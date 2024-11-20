bb2a4f2eef2eb54d2855f9c29efe3bbf66961ace
Naya Singhania <naya.singhania@gmail.com>
2024-11-20 14:50:04 -0800

    update migrations

--- drizzle/0000_nebulous_pride.sql
+++ drizzle/0000_nebulous_pride.sql
@@ -18,6 +18,14 @@ CREATE TABLE IF NOT EXISTS "professors" (
 	"department" text NOT NULL
 );
 --> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "reviews" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"rating" integer NOT NULL,
+	"review" text NOT NULL,
+	"course_id" integer NOT NULL,
+	"professor_id" integer NOT NULL
+);
+--> statement-breakpoint
 DO $$ BEGIN
  ALTER TABLE "professors_courses" ADD CONSTRAINT "professors_courses_professor_id_professors_id_fk" FOREIGN KEY ("professor_id") REFERENCES "public"."professors"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
@@ -26,6 +34,18 @@ END $$;
 --> statement-breakpoint
 DO $$ BEGIN
  ALTER TABLE "professors_courses" ADD CONSTRAINT "professors_courses_course_id_courses_id_fk" FOREIGN KEY ("course_id") REFERENCES "public"."courses"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "reviews" ADD CONSTRAINT "reviews_course_id_courses_id_fk" FOREIGN KEY ("course_id") REFERENCES "public"."courses"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "reviews" ADD CONSTRAINT "reviews_professor_id_professors_id_fk" FOREIGN KEY ("professor_id") REFERENCES "public"."professors"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
