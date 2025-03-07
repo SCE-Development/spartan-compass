000e6f416f1a4c24ce787ccce843bd8fa47ce88c
maixd7 <coolmichael08@gmail.com>
2025-03-07 11:08:46 -0800

    added sjsu class scraping

--- scraper/main.py
+++ scraper/main.py
@@ -1,4 +1,4 @@
-from scrapers import rmp
+from scrapers import rmp, sjsu
 
 def main():
   professors = rmp.get_professors()
@@ -7,6 +7,12 @@ def main():
     for professor in professors:
       print(professor)
     print(f"Total Professors: {len(professors)}")
+  classes = sjsu.get_classes()
+  if classes:
+    print("Professors:")
+    for c in classes:
+      print(c)
+    print(f"Total Classes: {len(classes)}")
 
 if __name__ == "__main__":
   main()
