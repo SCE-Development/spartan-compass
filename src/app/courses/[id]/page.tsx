b05a289069a3af066cfbb3335e584f1a4c8a6792
dvegas03_Desktop <khardan100@gmail.com>
2025-04-21 16:18:55 -0700

    Handle missing professors + db migrations
    - Add db migrations after adding "avg_rating" column
    - Add fallback to "No rating" after "avg_rating" is null/0 on the frontend
    Closes #57

--- src/app/courses/[id]/page.tsx
+++ src/app/courses/[id]/page.tsx
@@ -58,8 +58,14 @@ export default async function CoursePage({
                     </CardHeader>
                     <CardContent>
                       <div className="mt-2">
-                        {/* The actual rating is a placeholder, the professorsTable scheme has not been updated to have starRating as a field */}
-                        <StarRating rating={result.professor.avgRating ?? 0} textColor="text-muted-foreground" />
+                        {result.professor.avgRating ? (
+                          <StarRating
+                            rating={result.professor.avgRating}
+                            textColor="text-muted-foreground"
+                          />
+                        ) : (
+                          <span className="text-sm italic text-muted-foreground">No rating</span>
+                        )}
                       </div>
                     </CardContent>
                   </Card>
